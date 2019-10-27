1390c76c0b4430847068a008131ebf7c1595bb3b
Guilherme Carvalho <guibrancopc@gmail.com>
2019-10-27 19:04:41 -0300

    chore: Installing esling plugin for jest.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
+    jest: true,
   },
   extends: [
     'plugin:vue/essential',
@@ -14,6 +15,7 @@ module.exports = {
   },
   plugins: [
     'vue',
+    'jest'
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
